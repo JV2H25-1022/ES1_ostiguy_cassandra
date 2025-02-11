8ea60e8a830018c85a7d184558d19f7789416818
Cassostiguy <cassouostiguy@gmail.com>
2025-02-11 13:54:17 -0500

    ajout script pour redemarrer

--- Assets/Scripts/Parcours.cs
+++ Assets/Scripts/Parcours.cs
@@ -5,30 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Parcours : MonoBehaviour
 {
-//      bool Partie = true;
-//      int time = 5;
-//     // Start is called before the first frame update
-//     void Start()
-//     {
+    int Time = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
         
-//     }
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
+    // Update is called once per frame
+    void Update()
+    {
         
         
-//     }
+    }
     
-//     private void OnTriggerEnter(Collider col)
-//     {
-//         if (col.name == "Bille"){
-//             time-- * time.deltaTime;
-//             bool Partie = false;
-//         }
-//     }
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.CompareTag("Terminus")){
+            Invoke("Redemarrer", Time);
+        }
+    }
 
-    public void Redemarrer(){
+    void Redemarrer(){
     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
 }
